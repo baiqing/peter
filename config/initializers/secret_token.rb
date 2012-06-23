4277dded56ad623b6a09aa6c3a7e9bd3f6822060
peter <rongbaiqing@163.com>
2012-06-23 14:44:30 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microweibo::Application.config.secret_token = 'f16ed922b9b437fb2a7f499a288c57a1e17a0ea17c331bbfa743c4702333537bb5ba286809ff4f7e0676ae380bb4dce5080771a4881445cc74130c455be63aa2'
